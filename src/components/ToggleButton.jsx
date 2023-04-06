646b08e2449fe7cdcf60c0d7d41bd2a73069a5a6
Cinquin Andy <andy.cinquin@infotel.com>
2023-04-06 13:34:34 +0200

    prettier files wasn't already done

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,38 +1,38 @@
-import {useState} from "react";
-import {Switch} from "@headlessui/react";
-import clsx from "clsx";
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+import clsx from 'clsx'
 
-export function ToggleButton({onToggle}) {
-    const [enabled, setEnabled] = useState(true)
+export function ToggleButton({ onToggle }) {
+  const [enabled, setEnabled] = useState(true)
 
-    const handleToggle = (value) => {
-        setEnabled(value);
-        if (onToggle) {
-            onToggle(value);
-        }
+  const handleToggle = (value) => {
+    setEnabled(value)
+    if (onToggle) {
+      onToggle(value)
     }
+  }
 
-    return (
-        <Switch.Group as="div" className="flex items-center">
-            <Switch.Label as="span" className="mr-3 text-sm">
-                <span className="font-medium text-gray-900">Mode alternatif</span>
-            </Switch.Label>
-            <Switch
-                checked={enabled}
-                onChange={handleToggle}
-                className={clsx(
-                    enabled ? 'bg-indigo-600' : 'bg-gray-200',
-                    'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2'
-                )}
-            >
-            <span
-                aria-hidden="true"
-                className={clsx(
-                    enabled ? 'translate-x-5' : 'translate-x-0',
-                    'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
-                )}
-            />
-            </Switch>
-        </Switch.Group>
-    )
+  return (
+    <Switch.Group as="div" className="flex items-center">
+      <Switch.Label as="span" className="mr-3 text-sm">
+        <span className="font-medium text-gray-900">Mode alternatif</span>
+      </Switch.Label>
+      <Switch
+        checked={enabled}
+        onChange={handleToggle}
+        className={clsx(
+          enabled ? 'bg-indigo-600' : 'bg-gray-200',
+          'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2'
+        )}
+      >
+        <span
+          aria-hidden="true"
+          className={clsx(
+            enabled ? 'translate-x-5' : 'translate-x-0',
+            'pointer-events-none inline-block h-5 w-5  transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+          )}
+        />
+      </Switch>
+    </Switch.Group>
+  )
 }
