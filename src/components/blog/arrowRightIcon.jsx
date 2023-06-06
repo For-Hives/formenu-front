660b5ad9feaed42f0fdbb80de3d63e772461b349
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-06 20:37:18 +0200

    create components for all elements, split as atomic design

---
+++ src/components/blog/arrowRightIcon.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ArrowRightIcon(props) {
+    return (
+        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
+            <path
+                d="M7.25 4.75l3.5 3.25m0 0-3.5 3.25M11.75 8H3.25"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    )
+}
---
+++ src/components/blog/article.jsx
@@ -0,0 +1,18 @@
+import {Card} from "@/components/Card";
+import {formatDate} from "@/lib/formatDate";
+import React from "react";
+
+function Article({article}) {
+    return (
+        <Card as="article">
+            <Card.Title href={`/articles/${article.slug}`}>
+                {article.title}
+            </Card.Title>
+            <Card.Eyebrow as="time" dateTime={article.date} decorate>
+                {formatDate(article.date)}
+            </Card.Eyebrow>
+            <Card.Description>{article.description}</Card.Description>
+            <Card.Cta>Read article</Card.Cta>
+        </Card>
+    )
+}
