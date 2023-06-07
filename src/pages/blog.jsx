c0d779c0f4c92f74227095f64fedcd463c53bde7
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-07 11:47:48 +0200

    Rename

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,104 +1,103 @@
 import Head from 'next/head'
 import React from 'react'
-import {Container} from '@/components/Container'
-import {InstagramIcon, LinkedInIcon} from '@/components/SocialIcons'
-import {Header} from '@/components/Header'
+import { Container } from '@/components/Container'
+import { InstagramIcon, LinkedInIcon } from '@/components/SocialIcons'
+import { Header } from '@/components/Header'
 import Loader from '@/components/Loader'
-import {initPocketBase} from "../../lib/pocketbase";
-import {useQuery} from "@tanstack/react-query";
-import Photos from "@/components/blog/Photos";
-import Resume from "@/components/blog/Resume";
-import Newsletter from "@/components/blog/Newsletter";
-import SocialLink from "@/components/blog/SocialLink";
+import { initPocketBase } from '../../lib/pocketbase'
+import { useQuery } from '@tanstack/react-query'
+import Photos from '@/components/blog/Photos'
+import Resume from '@/components/blog/Resume'
+import Newsletter from '@/components/blog/Newsletter'
+import SocialLink from '@/components/blog/SocialLink'
 
 /**
  * Blog page
  */
 export default function Blog() {
-    const pb = initPocketBase()
+	const pb = initPocketBase()
 
-    /**
-     * Get articles, sort by created date, and limit to 3, then set to state
-     * use pocketbase & react-query to fetch data
-     * react-query will cache data
-     */
-    const {isLoading, isError, data, error} = useQuery({
-        queryKey: ['articles'],
-        queryFn: async () => {
-            return pb.collection('articles').getFullList({
-                sort: '-created',
-            })
-        },
-    })
+	/**
+	 * Get articles, sort by created date, and limit to 3, then set to state
+	 * use pocketbase & react-query to fetch data
+	 * react-query will cache data
+	 */
+	const { isLoading, isError, data, error } = useQuery({
+		queryKey: ['articles'],
+		queryFn: async () => {
+			return pb.collection('articles').getFullList({
+				sort: '-created',
+			})
+		},
+	})
 
-    if (isLoading) return <Loader/>
+	if (isLoading) return <Loader />
 
-    if (error) return 'An error has occurred: ' + error.message
+	if (error) return 'An error has occurred: ' + error.message
 
-    return (
-        <>
-            <Head>
-                <title>
-                    ForMenu le menu numérique qui va révolutionner votre restaurant
-                </title>
-                <meta
-                    name="description"
-                    content="ForMenu est l'application révolutionnaire pour votre carte de restaurant qui vous permet de créer une expérience unique pour vos clients."
-                />
-            </Head>
-            <Header></Header>
-            <Container className="mt-9">
-                <div className="max-w-2xl">
-                    <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                        ForMenu le menu numérique qui va révolutionner votre restaurant
-                    </h1>
-                    <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                        Nous sommes l’équipe de ForMenu, une équipe de jeune entrepreneur
-                        passionnés, basés à Nantes, dans ce blog nous vous donnerons des
-                        conseils et nous parlerons de nos dernières actualités, ainsi que de
-                        nos derniers changelogs et notre Roadmap générale !
-                    </p>
-                    <div className="mt-6 flex gap-6">
-                        <SocialLink
-                            href="https://instagram.com"
-                            aria-label="Follow on Instagram"
-                            icon={InstagramIcon}
-                        />
+	return (
+		<>
+			<Head>
+				<title>
+					ForMenu le menu numérique qui va révolutionner votre restaurant
+				</title>
+				<meta
+					name="description"
+					content="ForMenu est l'application révolutionnaire pour votre carte de restaurant qui vous permet de créer une expérience unique pour vos clients."
+				/>
+			</Head>
+			<Header></Header>
+			<Container className="mt-9">
+				<div className="max-w-2xl">
+					<h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+						ForMenu le menu numérique qui va révolutionner votre restaurant
+					</h1>
+					<p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+						Nous sommes l’équipe de ForMenu, une équipe de jeune entrepreneur
+						passionnés, basés à Nantes, dans ce blog nous vous donnerons des
+						conseils et nous parlerons de nos dernières actualités, ainsi que de
+						nos derniers changelogs et notre Roadmap générale !
+					</p>
+					<div className="mt-6 flex gap-6">
+						<SocialLink
+							href="https://instagram.com"
+							aria-label="Follow on Instagram"
+							icon={InstagramIcon}
+						/>
 
-                        <SocialLink
-                            href="https://linkedin.com"
-                            aria-label="Follow on LinkedIn"
-                            icon={LinkedInIcon}
-                        />
-                    </div>
-                </div>
-            </Container>
-            <Photos/>
-            <Container className="mt-24 md:mt-28">
-                <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-                    <div className="flex flex-col gap-16">
-                        {
-                            /**
-                             * map sur les articles
-                             */
-                            data.map((article) => (
-                                // fragment : permet de ne pas avoir de div en plus dans le dom & de wrap les éléments
-                                <div key={article.id}>
-                                    <p className="text-sm text-zinc-500 dark:text-zinc-400">
-                                        {article.id}
-                                    </p>
-                                    <h2>{article.title}</h2>
-                                </div>
-                            ))
-
-                        }
-                    </div>
-                    <div className="space-y-10 lg:pl-16 xl:pl-24">
-                        <Newsletter/>
-                        <Resume/>
-                    </div>
-                </div>
-            </Container>
-        </>
-    )
+						<SocialLink
+							href="https://linkedin.com"
+							aria-label="Follow on LinkedIn"
+							icon={LinkedInIcon}
+						/>
+					</div>
+				</div>
+			</Container>
+			<Photos />
+			<Container className="mt-24 md:mt-28">
+				<div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+					<div className="flex flex-col gap-16">
+						{
+							/**
+							 * map sur les articles
+							 */
+							data.map(article => (
+								// fragment : permet de ne pas avoir de div en plus dans le dom & de wrap les éléments
+								<div key={article.id}>
+									<p className="text-sm text-zinc-500 dark:text-zinc-400">
+										{article.id}
+									</p>
+									<h2>{article.title}</h2>
+								</div>
+							))
+						}
+					</div>
+					<div className="space-y-10 lg:pl-16 xl:pl-24">
+						<Newsletter />
+						<Resume />
+					</div>
+				</div>
+			</Container>
+		</>
+	)
 }
